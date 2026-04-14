1909c04670b890a748150e2805545245dfeffd94
Shivam Tiwari <shivam@Shivams-Air.lan>
2026-04-14 20:11:38 +0530

    footer copyright text change

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <footer className="w-full px-4 sm:px-6 py-6">
             <div className="max-w-7xl mx-auto flex items-center justify-between text-[#6E6C81] text-[14px]">
-                <div>&copy; {new Date().getFullYear()}, The Carbonated Body (co2body.com), a product of Carbogenetics LLC. All Rights Reserved | Made by CV Infotech</div>
+                <div>&copy; {new Date().getFullYear()}, CO2Body (The Carbonated Body), Carbogenetics LLC. All Rights Reserved</div>
                 <div className="flex gap-4">
                     <Link to="/terms" className="hover:underline">Terms & Conditions</Link>
                     <Link to="/privacy" className="hover:underline">Privacy Policy</Link>
